4affa96e2e50ee21a2a4fd4adc481b3c97560cf7
Bob <updatesvc@gmail.com>
2022-09-14 01:19:25 +0300

    Create noclose.js

---
+++ grpc-tests/noclose.js
@@ -0,0 +1,22 @@
+import grpc from 'k6/net/grpc';
+import { check } from 'k6';
+
+const client = new grpc.Client();
+const GRPC_SERVER = "localhost:4000"
+client.load(['../definitions'], 'user.proto');
+
+export default () => {
+    if (__ITER == 0) {
+        client.connect(GRPC_SERVER, {
+            plaintext: true, //allow insecure connection
+            timeout: "2s"
+        });
+    }
+    const response = client.invoke('userPackage.Billing/readUser', {});
+
+    check(response, {
+        'status is OK': (r) => r && r.status === grpc.StatusOK,
+    });
+    // Do NOT close the client
+};
+//k6 run grpc-tests/noclose.js --duration 10s --vus=1
