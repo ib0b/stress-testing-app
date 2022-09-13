61c9bad78fe4d0abb9f9bc443228ed5361bbd77e
Bob <updatesvc@gmail.com>
2022-09-13 19:14:11 +0300

    update docker

--- docker.js
+++ docker.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 const PORT = process.env.PORT || 3000
-console.log(`env port is ${process.env.PORT}`)
+
 app.listen(PORT, () => console.log(`listeing on port ${PORT}`))
 
 app.get("/", (req, res) => res.send({
