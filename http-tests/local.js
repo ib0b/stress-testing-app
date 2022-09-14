ba84f7c04bd8dcdae37d5ea717d5509e6f2279ff
Bob <updatesvc@gmail.com>
2022-09-14 19:17:59 +0300

    Update local.js

--- http-tests/local.js
+++ http-tests/local.js
@@ -1,11 +1,11 @@
 import http from 'k6/http';
-
+import config from '../config.js'
 export const options = {
     vus: 1000,
     duration: '30s',
 };
 export default function () {
-    http.get('http://localhost:3000/');
+    http.get(config.LOCAL_HTTP_HOST);
 }
 
 //k6 run http-tests/test.js --duration=30s --vus=32
