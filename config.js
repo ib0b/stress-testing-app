ca38e7b32bcfca9eb00bf9ad6778b64a94fbbbdb
Bob <updatesvc@gmail.com>
2022-09-14 19:14:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    REMOTE_HTTP_HOST: "http://172.31.43.204:3000", //change this to you local instance IP
-    REMOTE_GRPC_HOST: "172.31.43.204:4000" //change this to you local instance IP, it should not have http since its GRPC   
+    REMOTE_HTTP_HOST: "http://172.31.43.204:3000", //change this to you remote instance IP
+    REMOTE_GRPC_HOST: "172.31.43.204:4000", //change this to you remote instance IP, it should not have http since its GRPC   
 }
 
