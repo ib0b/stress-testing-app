f0b5387099f547360bbe1ebb4bda6be12ac42e51
Bob <updatesvc@gmail.com>
2022-09-14 12:26:20 +0300

    Create remote.js

---
+++ http-tests/remote.js
@@ -0,0 +1,10 @@
+import http from 'k6/http';
+import config from "../config.js"
+export const options = {
+    vus: 1000,
+    duration: '30s',
+};
+export default function () {
+    http.get(config.REMOTE_HTTP_HOST);
+}
+
