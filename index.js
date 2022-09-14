56ce4ffa523f8f85f55ccf6246984d7fd127229b
Bob <updatesvc@gmail.com>
2022-09-14 12:27:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+//standalone server without docker
 const express = require('express')
 
 const app = express()
 const PORT = 3000
+const UUID = new Date().getTime() + randomInt(1, 1000) //server unique id
 app.listen(PORT, () => console.log(`listeing on port ${PORT}`))
 
 app.get("/", (req, res) => res.send({
     username: "Bob",
     premium: true,
     nextBillingDate: "01-01-2023",
-    country: "KE"
+    country: "KE",
+    port: PORT,
+    UUID
 }))
+
+function randomInt(min, max) {
+    return min + Math.floor(Math.random() * (max - min))
+}
